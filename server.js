8cc7cc8e335fd6a59a2d2eba59cb1b670e6e9f64
Curtis Lytle <curtlytle@gmail.com>
2017-12-18 13:15:43 -0700

    setting up server and default home page

--- server.js
+++ server.js
@@ -1,3 +1,19 @@
 var express = require("express");
 var bodyParser = require("body-parser");
 var path = require("path");
+
+var app = express();
+var PORT = 3000;
+
+// Sets up the Express app to handle data parsing
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
+
+app.get("/", function (req, res) {
+    res.sendFile(path.join(__dirname, "/app/public/home.html"));
+});
+
+
+app.listen(PORT, function () {
+    console.log("App listening on PORT " + PORT);
+});
